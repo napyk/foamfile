d5b575c35f3461b776b91b5640d20f7eae8a30de
nkp <17582076+napyk@users.noreply.github.com>
2019-10-10 17:22:00 +0200

    small changes for PyPi release

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name="foamfile",
-      version="0.1",
+      version="0.11",
       description='OpenFOAM config file parser',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author="Nikolas Pfister",
       author_email="pfister.nikolas@gmail.com",
       packages=find_packages(),
+      license="MIT",
+      url="https://github.com/napyk/foamfile",
       install_requires=required)
